c0f7e7bfb097a03123a58e1f646e7f0737a0bc52
Mahi-Abs <m1606093@edu.misis.ru>
2023-07-17 04:11:31 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char alp[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 	{
-		putchar(alp(i));
+		putchar(alp[i]);
 
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
